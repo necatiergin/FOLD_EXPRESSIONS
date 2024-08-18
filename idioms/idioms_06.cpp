bcd7f442c4b52202ebe4952718ae28f953482782
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-08-18 13:43:37 +0300

    Create idioms_06.cpp

---
+++ idioms/idioms_06.cpp
@@ -0,0 +1,14 @@
+//fold get last
+
+#include <iostream>
+
+template <typename ...Ts>
+auto get_last(Ts ...args)
+{
+    return (args, ...);
+}
+
+int main()
+{
+    std::cout << get_last(1, 3, 7);
+}
