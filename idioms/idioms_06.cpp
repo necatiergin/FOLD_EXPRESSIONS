8f92ad407c3e1dea6aa302a6dd022d52d0108c8a
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-08-18 13:42:12 +0300

    Create idioms_06.cpp

---
+++ idioms/idioms_06.cpp
@@ -0,0 +1,16 @@
+//fold get first
+
+#include <iostream>
+
+template <typename ... Ts>
+auto get_first(Ts... ts)
+{
+    std::common_type_t<decltype(ts)...> result;
+    ((result = ts, true) || ...);
+    return result;
+}
+
+int main()
+{
+    std::cout << get_first(1.5, 3, 7);
+}
